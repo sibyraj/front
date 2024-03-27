abffd69e0cfb31e9aaf47354b898a90d9243be87
sibyraj <sibyraj2@gmail.com>
2024-03-27 18:52:41 +0530

    first commi

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = 3001;
@@ -12,10 +13,10 @@ app.use(bodyParser.json());
 // Create MySQL pool
 const pool = mysql.createPool({
   connectionLimit: 10, // Adjust as needed
-  host: 'localhost',
-  user: 'root',
-  password: '4100',
-  database: 'project'
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 });
 app.post('/api/login', (req, res) => {
   const { email, password } = req.body;
